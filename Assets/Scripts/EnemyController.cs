680a9200a2b2b7312f6d04753e14afcea1303a99
Michael Haungs <mhaungs@calpoly.edu>
2025-01-16 08:11:07 -0800

    Add input action
    Added an input action and also cleaned up EnemyController script.

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -5,12 +5,6 @@ public class EnemyController : MonoBehaviour
     public float speed;
     public float zBoundary;
 
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
         transform.Translate(-speed * Time.deltaTime * Vector3.forward);        
