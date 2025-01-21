eda2db5c49070cfa2abebd7fa75c9bc1b2156bb9
Michael Haungs <mhaungs@calpoly.edu>
2025-01-21 09:17:47 -0800

    Small Changes
    Just a quick save to sync.

--- Assets/Scripts/EnemyManager.cs
+++ Assets/Scripts/EnemyManager.cs
@@ -18,13 +18,6 @@ public class EnemyManager : MonoBehaviour
 
     float _timeSinceLastSpawn = 0f;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
     void Update()
     {
         _timeSinceLastSpawn += Time.deltaTime;
